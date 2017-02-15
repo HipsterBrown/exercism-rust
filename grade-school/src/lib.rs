72b67bc20440583b06b774af91a0084b0193a4b4
Nick Hehr <headhipster@hipsterbrown.com>
2017-02-15 14:28:57 -0500

    feat(grade-school): working implementation

--- grade-school/src/lib.rs
+++ grade-school/src/lib.rs
@@ -1,26 +1,30 @@
-use std::collections::HashMap;
+use std::collections::BTreeMap;
+use std::vec;
+
+type GradeLevel = vec::Vec<String>;
 
 pub struct School {
-    grades: HashMap<u32, Vec<String>>,
+    grades: BTreeMap<u32, Vec<String>>,
 }
 
 impl School {
     pub fn new() -> Self {
         School { 
-            grades: HashMap::new(),
+            grades: BTreeMap::new(),
         }
     }
 
     pub fn add(&mut self, level: u32, name: &str) {
-        unimplemented!();
-        // self.grades.entry(level).or_insert(Vec::new()).push(name); 
+        let grade = self.grades.entry(level).or_insert(GradeLevel::new()); 
+        grade.push(name.to_string());
+        grade.sort();
     }
 
     pub fn grades(&self) -> Vec<u32> {
-        self.grades.keys().cloned().collect()
+        self.grades.keys().cloned().collect::<Vec<u32>>()
     }
 
-    pub fn grade(&self, level: u32) -> Iterator<Item=Vec<String>> {
-        unimplemented!();
+    pub fn grade(&self, level: u32) -> Option<&GradeLevel> {
+        self.grades.get(&level)
     }
 }
